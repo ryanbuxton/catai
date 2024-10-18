8b7dfaf89519b255b6cbe136f22596b4ac5122a3
ryanbuxton <ryanballenger15@gmail.com>
2024-10-18 11:37:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const message = 'Cat AI'; // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message;
+
+// Log to console
+console.log(message);
+
+function hey(){
+
+
+      document.getElementById('textID').innerHTML = document.getElementById('textID').innerHTML + ' Meow. '
+
+
+    }
+
+
+function clean(){
+      document.getElementById('ta').value = ' '
+           document.getElementById('textID').innerHTML = '  '
+   
+    }
+
+
+function exam(){
+
+      document.getElementById('ta').value = 'Where are the mice at?'
+
+hey();
+    
+
+    
+    }
